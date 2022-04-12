f259ef059d1165583de739b5e6bcd9dee2c3b3df
Bailey <peybailey@FVFZMYE6J1WK.tld>
2022-04-12 14:51:23 -0500

    Third Commit. 4-12-22. Added Computer Cards & Stylized Card Button.

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 function run() {
-    getCard()
+    playerCard()
+    computerCard()
+    playerTotal()
+    computerTotal()
 }
 
-function getCard() {
+function playerCard() {
     var r = Math.floor(Math.random() * 10) + 1
     var suitArray = ["C", "D", "H", "S"]
     var randomSuit = Math.floor(Math.random() * 4) // 0 to 3
-    document.getElementById("card").insertAdjacentHTML("beforeend", "<img src=cards/" + r + suitArray[randomSuit] + ".jpg>")
+    document.getElementById("player").insertAdjacentHTML("beforeend", "<img src=Cards/" + r + suitArray[randomSuit] + ".jpg>")
+} 
+
+function computerCard() {
+    var c = Math.floor(Math.random() * 10) + 1
+    var suitArray2 = ["C", "D", "H", "S"]
+    var randomSuit2 = Math.floor(Math.random() * 4) // 0 to 3
+    document.getElementById("computer").insertAdjacentHTML("beforeend", "<img src=Cards/" + c + suitArray2[randomSuit2] + ".jpg>")
+}
+
+function playerTotal() {
+    let a = document.getElementById("pTotal")
+    a.innerHTML = "Your Total is " + r + "."
+}
+
+function computerTotal() {
+    let b = document.getElementById("cTotal")
+    b.innerHTML = "Computer Total is " + c + "."
 }

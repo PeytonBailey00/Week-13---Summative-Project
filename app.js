83e705d2cf92d588a27982bf0bc59a0651e8bfa5
Bailey <peybailey@FVFZMYE6J1WK.tld>
2022-04-13 14:54:13 -0500

    Fourth Commit. 4-13-22. Semi-Added No Cards Button & Fixed Ace Problem.

--- app.js
+++ app.js
@@ -1,30 +1,25 @@
 function run() {
     playerCard()
     computerCard()
-    playerTotal()
-    computerTotal()
 }
 
 function playerCard() {
     var r = Math.floor(Math.random() * 10) + 1
     var suitArray = ["C", "D", "H", "S"]
-    var randomSuit = Math.floor(Math.random() * 4) // 0 to 3
-    document.getElementById("player").insertAdjacentHTML("beforeend", "<img src=Cards/" + r + suitArray[randomSuit] + ".jpg>")
+    var randomSuit = Math.floor(Math.random() * 4)
+    document.getElementById("player").insertAdjacentHTML("beforeend", "<img src=images/" + r + suitArray[randomSuit] + ".jpg>")
+    return r
 } 
 
 function computerCard() {
     var c = Math.floor(Math.random() * 10) + 1
-    var suitArray2 = ["C", "D", "H", "S"]
-    var randomSuit2 = Math.floor(Math.random() * 4) // 0 to 3
-    document.getElementById("computer").insertAdjacentHTML("beforeend", "<img src=Cards/" + c + suitArray2[randomSuit2] + ".jpg>")
-}
+    var suitArrayC = ["C", "D", "H", "S"]
+    var randomSuitC = Math.floor(Math.random() * 4)
+    document.getElementById("computer").insertAdjacentHTML("beforeend", "<img src=images/" + c + suitArrayC[randomSuitC] + ".jpg>")
+    return c
+} 
 
 function playerTotal() {
-    let a = document.getElementById("pTotal")
-    a.innerHTML = "Your Total is " + r + "."
-}
-
-function computerTotal() {
-    let b = document.getElementById("cTotal")
-    b.innerHTML = "Computer Total is " + c + "."
+    let a = r
+    document.getElementById("pTotal").innerHTML = "Player Total is " + r + "."
 }

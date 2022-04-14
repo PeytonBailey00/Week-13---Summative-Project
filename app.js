c989fd988f69cc6f5b7fae111c2638f8fe789078
Bailey <peybailey@FVFZMYE6J1WK.tld>
2022-04-14 14:53:23 -0500

    Fifth Commit. 4-14-22. Added Reload Button & Created Baseline for Comparison Function.

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
 function run() {
     playerCard()
     computerCard()
+    playerTotal()
+    noMoreCards()
 }
 
 function playerCard() {
     var r = Math.floor(Math.random() * 10) + 1
     var suitArray = ["C", "D", "H", "S"]
     var randomSuit = Math.floor(Math.random() * 4)
-    document.getElementById("player").insertAdjacentHTML("beforeend", "<img src=images/" + r + suitArray[randomSuit] + ".jpg>")
+    let x = document.getElementById("player")
+    x.insertAdjacentHTML("beforeend", "<img src=images/" + r + suitArray[randomSuit] + ".jpg>")
     return r
 } 
 
 function computerCard() {
-    var c = Math.floor(Math.random() * 10) + 1
+    var rC = Math.floor(Math.random() * 10) + 1
     var suitArrayC = ["C", "D", "H", "S"]
     var randomSuitC = Math.floor(Math.random() * 4)
-    document.getElementById("computer").insertAdjacentHTML("beforeend", "<img src=images/" + c + suitArrayC[randomSuitC] + ".jpg>")
-    return c
+    let y = document.getElementById("computer")
+    y.insertAdjacentHTML("beforeend", "<img src=images/" + rC + suitArrayC[randomSuitC] + ".jpg>")
+    return rC
 } 
 
-function playerTotal() {
-    let a = r
-    document.getElementById("pTotal").innerHTML = "Player Total is " + r + "."
+function noMoreCards() {
+    let a = document.getElementById("pTotal")
+    let b = document.getElementById("cTotal")
+
+    a.innerHTML = "Player Total is " + r + "."
+    b.innerHTML = "Computer Total is " + rC + "."
+}
+
+function reload() {
+    document.location.reload()
 }
